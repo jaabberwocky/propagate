47994c19e3d0396f478654937b04bd53b6ba5250
tobias <tobias@tobias-leong.dev>
2023-08-28 22:29:32 +0800

    Add generate_dataset function

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
+use rand::Rng;
+
 #[derive(Debug)]
 pub struct Dataset {
     columns: Vec<Column>,
     rows: i32,
+    sep: String,
 }
 
 impl Dataset {
-    pub fn build(column_names: Vec<String>, ranges: Vec<String>, rows: i32) -> Self {
+    pub fn build(column_names: Vec<String>, ranges: Vec<String>, rows: i32, sep: String) -> Self {
         Dataset::check_args_length(&column_names, &ranges);
 
         let mut columns: Vec<Column> = Vec::new();
@@ -19,7 +22,7 @@ impl Dataset {
             };
             columns.push(column);
         }
-        Dataset { columns, rows }
+        Dataset { columns, rows, sep }
     }
 
     fn parse_range(range: &str) -> (i32, i32) {
@@ -37,6 +40,28 @@ impl Dataset {
             _ => (),
         }
     }
+
+    pub fn generate_dataset(&self) -> Vec<String> {
+        let mut generated_dataset: Vec<String> = Vec::new();
+
+        // add column names
+        let column_header: Vec<String> = self.columns.iter().map(|c| c.name.clone()).collect();
+        generated_dataset.push(column_header.join(&self.sep));
+
+        for _ in 0..self.rows {
+            let mut row: Vec<String> = Vec::new();
+
+            for column in &self.columns {
+                let ColumnDataType::Int(min) = &column.min;
+                let ColumnDataType::Int(max) = &column.max;
+                let value = rand::thread_rng().gen_range(*min..=*max).to_string();
+                row.push(value);
+            }
+            generated_dataset.push(row.join(&self.sep));
+        }
+
+        generated_dataset
+    }
 }
 
 #[derive(Debug)]
@@ -48,6 +73,5 @@ struct Column {
 
 #[derive(Debug)]
 enum ColumnDataType {
-    Float(f32),
     Int(i32),
 }
